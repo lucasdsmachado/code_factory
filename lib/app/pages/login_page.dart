88d48d005d677c249e650b7246f5807d893b66ef
Lucas Moreno <lmorenooitabem@gmail.com>
2024-07-05 20:09:28 -0300

    fix: Correção do overflow

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -18,68 +18,71 @@ class LoginPage extends StatelessWidget {
     // Obter a largura e a altura da tela
 
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Center(
-            child: Image.asset(
-              "assets/images/login_image.png",
-              // width: screenWidth * 0.70,
-              height: MediaQuery.of(context).size.height * 0.40,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 40,
             ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const TextInput(labelTxt: "Email"),
-                const SizedBox(
-                  height: 15,
-                ),
-                const PasswordInput(isSignUp: false),
-                const SizedBox(
-                  height: 60,
-                ),
-                GenericButton(
-                  buttonText: "Log in",
-                  onPressedFunction: () {
-                    if (_formKey.currentState!.validate()) {
-                      Navigator.pushReplacement(
+            Center(
+              child: Image.asset(
+                "assets/images/login_image.png",
+                // width: screenWidth * 0.70,
+                height: MediaQuery.of(context).size.height * 0.40,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const TextInput(labelTxt: "Email"),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const PasswordInput(isSignUp: false),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  GenericButton(
+                    buttonText: "Log in",
+                    onPressedFunction: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const AccountPages(), // TODO: validar login
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => const AccountPages(), // TODO: validar login
+                          builder: (context) => SignupPage(),
                         ),
                       );
-                    }
-                  },
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>  SignupPage(),
+                    },
+                    child: const Text(
+                      "Registrar",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontVariations: [FontVariation("wght", 500)],
+                        fontWeight: FontWeight.bold,
                       ),
-                    );
-                  },
-                  child: const Text(
-                    "Registrar",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontVariations: [FontVariation("wght", 500)],
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
