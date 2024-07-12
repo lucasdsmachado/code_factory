1ac7ec2eb57e313501f00bf6a1fd455df76a8df9
lucasdsmachado <briandsmachado@gmail.com>
2024-07-12 13:17:39 -0300

    feat: adiciona firebase database e auth ao projeto

--- android/app/src/main/kotlin/com/example/code_factory/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/code_factory/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.code_factory
+package codefactory.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,19 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:code_factory/app/controller/theme_provider.dart';
 import 'package:code_factory/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:code_factory/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Carregar o tema inicial do SharedPreferences
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ChangeNotifierProvider(
