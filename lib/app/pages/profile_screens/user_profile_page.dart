9437a08dc12b5349f7554767370142c894c37cd8
lucasdsmachado <briandsmachado@gmail.com>
2024-07-15 14:16:27 -0300

    feat: adiciona função para deslogar usuario

--- lib/app/pages/profile_screens/user_profile_page.dart
+++ lib/app/pages/profile_screens/user_profile_page.dart
@@ -3,6 +3,7 @@ import 'package:code_factory/app/pages/login_page.dart';
 import 'package:code_factory/app/pages/payment_page.dart';
 import 'package:code_factory/app/widgets/others/header.dart';
 import 'package:code_factory/app/widgets/buttons/profile_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserProfilePage extends StatelessWidget {
@@ -65,12 +66,10 @@ class UserProfilePage extends StatelessWidget {
             height: 15,
           ),
           TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            LoginPage())); // TODO: sair do app
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => LoginPage()));
               },
               child: const Text(
                 "Sair do aplicativo",
