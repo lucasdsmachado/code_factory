2b029ad0157a2b881bd23d84cb366dbfc2460bc5
Lucas Moreno <lmorenooitabem@gmail.com>
2024-07-20 20:54:42 -0300

    fix: resolução de conflitos

--- lib/app/pages/profile_screens/home_page.dart
+++ lib/app/pages/profile_screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:code_factory/app/pages/course_pages/product_detail.dart';
+import 'package:code_factory/app/utils/firebase_helper.dart';
 import 'package:code_factory/app/widgets/others/categories.dart';
 import 'package:code_factory/app/widgets/cards/course_card.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -48,8 +49,7 @@ class HomePageState extends State<HomePage> {
         });
       }
     } catch (e) {
-      // TODO: Adicione um tratamento de erros
-      print("Erro ao carregar cursos: $e");
+      showErrorSnackBar(context, 'Erro ao carregar cursos $e');
     }
   }
 
